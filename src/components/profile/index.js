49227362c52db8ed3f85d225745b6e0be7c576b4
alnstien <alnstien@yahoo.com>
2021-04-19 18:07:26 +0800

    commited any changes

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -7,12 +7,122 @@ export default function Profile(props) {
     return (
     <div className="con-container"> 
         <Sidebar profileActive={true}/>
-        <div className="content-wrapper">
-          
             <div className="main-content-wrapper">
                 <div className="content-main-holder">
-                    <div className="wrapper-main">
-                        <div className="profile-wrapper">
+                <div className="form-group-profile">
+                                <div className="profile-picture">
+                                    <button className="edit-profile-picture-btn"><AiFillCamera size={24} /></button>
+                                </div>
+                                <div className="profile-info-me">
+                                    <h4 className="profile-intro">PERSONAL INFORMATION</h4>
+                                    <div style={{paddingBottom:'10px'}}>
+                                        <h3>Al Jimson A. Megrino</h3>
+                                        <p>email@email.com</p>
+                                        <div className="profile-role">
+                                        <h5>Administrator</h5>
+                                    </div>
+                                    </div>
+                                </div>
+                                <button className="edit-btn"><MdEdit size={24} /></button>
+                            </div>
+                            <form>
+                                <fieldset className="fieldset">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <fieldset className="fieldset">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <div className="form-inline-group-profile">
+                                <fieldset className="fieldset mr-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <fieldset className="fieldset ml-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                </div>
+                                <div className="form-inline-group-profile">
+                                <fieldset className="fieldset mr-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <fieldset className="fieldset ml-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                </div>
+                                <div className="form-inline-group-profile">
+                                <fieldset className="fieldset mr-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <fieldset className="fieldset ml-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                </div>
+                                <div className="form-inline-group-profile">
+                                <fieldset className="fieldset mr-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                <fieldset className="fieldset ml-5">
+                                    <legend className="form-label">Address</legend>
+                                    <input 
+                                        value="Iligan city"
+                                        className="form-input"
+                                    />
+                                    <button className="fieldset-btn"><MdEdit size={24} /></button>
+                                </fieldset>
+                                </div>
+
+                            </form>
+                </div>
+            </div>
+    </div>
+    )
+}
+
+{/* <div className="profile-wrapper">
                             <div className="form-group-profile">
                                 <div className="profile-picture">
                                     <button className="edit-profile-picture-btn"><AiFillCamera size={24} /></button>
@@ -118,14 +228,6 @@ export default function Profile(props) {
                                     <button className="fieldset-btn"><MdEdit size={24} /></button>
                                 </fieldset>
                                 </div>
-                                
-                                
+
                             </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    )
-}
+                        </div> */}
