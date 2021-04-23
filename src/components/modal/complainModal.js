336d59b8b62e1d3a4a1c33f96a30580e991651f0
alnstien <alnstien@yahoo.com>
2021-04-23 15:41:09 +0800

    committed

--- src/components/modal/complainModal.js
+++ src/components/modal/complainModal.js
@@ -1,92 +1,17 @@
 import React from 'react'
 import Modal from 'react-modal'
 import './modal.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { TextareaAutosize,TextField } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-}));
 
-function getSteps() {
-  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
-}
 
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return 'Select campaign settings...';
-    case 1:
-      return 'What is an ad group anyways?';
-    case 2:
-      return 'This is the bit I really care about!';
-    default:
-      return 'Unknown step';
-  }
-}
+
 
 export default function ComplainModal({show,setShow}) {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
-
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
- 
   
+  const handleClose = ()=>{
+    return setShow(false)
+  }
   return (
       <Modal isOpen={show}
       style={{
@@ -118,7 +43,27 @@ export default function ComplainModal({show,setShow}) {
       <div className="modal-wrapper">
           <div className="modal-contain">
               <div className="complain-modal-wrapper">
-
+              <div className="modal-header">
+                  <h2 className="modal-title"></h2>
+                  <button 
+                    onClick={handleClose}
+                    className="modal-close"
+                  >&times;</button>
+              </div>
+              <div className="modal-content">
+                  <form className="form-flex">
+                  <TextField 
+                      variant="outlined"
+                      label="Last Name"
+                      // onChange={e=>handleChange(e,setLname)}
+                  />
+                  <TextareaAutosize 
+                      rowsMin={8}
+                      rowsMax={8}
+                      placeholder="Write something..."
+                    />
+                  </form>
+              </div>
               </div>
           </div>
       </div>
