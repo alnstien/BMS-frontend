5eb50a8c00d8f2a08d7dffdd0bea0b36e2a8f451
alnstien <alnstien@yahoo.com>
2021-04-23 22:19:07 +0800

    commited

--- src/components/modal/createPost.js
+++ src/components/modal/createPost.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
 import './modal.css';
-import 'simplebar'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 import { TextareaAutosize } from '@material-ui/core';
 import {BiImageAdd} from 'react-icons/bi'
 import { Button } from '@material-ui/core';
 
+function PhotoHolder({preview,handleRemovePhoto,index}){
+  return(
+    <div className="posts-photo-holder">
+        <button 
+          onClick={()=>handleRemovePhoto(preview,index)}
+          className="posts-photo-close">&times;</button>
+        <img src={preview} alt=""/>
+    </div>
+  )
+}
+
+
 export default function CreatePostModal({show,setShow}) {
-  
+  const [photos,setPhotos] = useState([])
+  const [previews, setPreviews] = useState([]);
+  const [files, setFiles] = useState([]);
+
   const handleClose=()=>{
     return setShow(false)
   }
+
+  const handleImage=(e)=>{
+    const fileList = Array.from(e.target.files);
+    setFiles(fileList);
+    const mappedFiles = fileList.map((file) => ({
+      ...file,
+      preview: URL.createObjectURL(file),
+    }));
+    setPreviews(mappedFiles)
+}
+
+  const handleRemovePhoto=(img,n)=>{
+    const newPreviews = previews.filter(preview=>preview.preview !== img);
+    const newFiles = files.filter((file,index)=>index !== n);
+    setPreviews(newPreviews);
+    setFiles(newFiles);
+  }
+
   return (
       <Modal isOpen={show}
       style={{
@@ -49,13 +83,31 @@ export default function CreatePostModal({show,setShow}) {
               </div>
               <div className="modal-body">
                 <div className="modal-adons">
-                    <input type="file" id="post-img" />
-                    <label className="post-img-wrapper" for="post-img">
+                    <input 
+                      type="file" 
+                      multiple={true}
+                      id="post-img" 
+                      onChange={e=>handleImage(e)}
+                    />
+                    <label className="post-img-wrapper"
+                      htmlFor="post-img">
                         <BiImageAdd size={25} color={'#fff'} />
                     </label>
-                    <div data-simplebar className="modal-photos-wrapper">
-                            <p>wdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwddwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwddwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwdwwdwd</p>
-                    </div>
+                    {
+                      previews.length <=0 ? null :
+                      <SimpleBar className="modal-photos-wrapper">
+                      {
+                        previews.map((item,index)=>{
+                          return <PhotoHolder
+                            key={index}
+                            index={index}
+                            preview={item.preview}
+                            handleRemovePhoto={handleRemovePhoto}
+                          />
+                        })
+                      }
+                    </SimpleBar>
+                    }
                 </div>
                   <form style={{marginTop:10}}>
                     <TextareaAutosize 
