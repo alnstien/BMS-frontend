992801f134445ef1f6e895515b04a2ff64c7a6a5
alnstien <alnstien@yahoo.com>
2021-05-04 14:03:49 +0800

    committed

---
+++ src/components/transaction/listItem.js
@@ -0,0 +1,43 @@
+function ListItem({status,request,checked,handleCheck,handleProcess}){
+    const [selected,setSelected] = useState(checked);
+
+    const handleSelect=useCallback(()=>{
+        setSelected(prevSelect=>!prevSelect)
+    },[checked])
+
+    return(
+        <>
+            <tr className="tb-tr">
+                {
+                     status ? 
+                        <td>
+                            <input 
+                                className="req-check"
+                                type="checkbox" 
+                                checked={selected ? true : false}
+                                onChange={handleSelect}
+                                value={request.id}
+                            />
+                        </td>
+                    :null
+                }
+                <td>{request.date}</td>
+                <td>{request.name}</td>
+                <td>{request.request}</td>
+                <td>
+                    <div className="request-status-cc">
+                        <p
+                            style={{color: request.status==='Valid' ? 'green' :'rgb(231, 12, 12)' }}
+                        >{request.status}</p>
+                    </div>
+                </td>
+                <td>
+                    <button 
+                        className="process-btn"
+                        onClick={handleProcess}
+                    >Process</button>
+                </td>
+            </tr>
+        </>
+    )
+}
