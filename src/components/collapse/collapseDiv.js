51d7cada875f9afa3e115b084ebbf980048fc388
alnstien <alnstien@yahoo.com>
2021-05-16 19:19:45 +0800

    added some changes

---
+++ src/components/collapse/collapseDiv.js
@@ -0,0 +1,102 @@
+import { TextField, Typography } from '@material-ui/core';
+import React,{useState,useRef,useEffect} from 'react'
+import ClickOutsideHandler from '../../config/clickOutsideHandler';
+import {IoIosArrowDown} from 'react-icons/io'
+import {BsTrashFill} from 'react-icons/bs'
+import {MdModeEdit} from 'react-icons/md'
+
+export default function CollapseDiv({member}) {
+
+    
+    return (
+    <div className="flex-div" >
+        <div className="collapse-div-heading">
+            <div className="collapse-div-heading-header">
+                <Typography variant="body1">{member.role}</Typography>
+            </div>
+            <div>
+                <CollapseField 
+                    member={member}
+                />
+            </div>
+        </div>
+    </div>
+    )
+}
+
+function CollapseField({member}){
+    const [show,setShow] = useState(false)
+    const myRef = useRef();
+    //clicking outside element handler
+    ClickOutsideHandler(myRef,useEffect,setShow)
+
+    const toggleShow=(e)=>{
+        e.preventDefault()
+        setShow(!show)
+      }
+
+    const styles = {
+        collapseStyle:{
+            height: show ? "200px" : 0,
+            padding: show ? "10px": 0,
+            transition:'.1s',
+            border: show ? "1px solid lightgray": null
+        },
+        toggleButton:{
+            
+        }
+    }
+
+
+    return(
+        <>
+        <div  ref={myRef}>
+            <div className="collapse-div-heading-content">
+                <TextField 
+                    id="outlined-basic" 
+                    variant="outlined"
+                    color="secondary"
+                    value={member.name}
+                    disabled={true}
+                    className="collapse-input"
+                />
+                <button 
+                    className="collapse-btn"
+                    onClick={e=>toggleShow(e)}
+                >
+                    <span 
+                        style={{
+                            transition:".2s",
+                            transform: show ? 'rotate(180deg)' : null
+                        }}
+                    >
+                        <IoIosArrowDown size={24}/>
+                    </span>
+                </button>
+                <button 
+                    className="collapse-btn"
+                    // onClick={e=>toggleShow(e)}
+                >
+                    <MdModeEdit size={24} color="#5924F0" />
+                </button>
+                <button 
+                    className="collapse-btn"
+                    // onClick={e=>toggleShow(e)}
+                >
+                    <BsTrashFill size={24} color="#EE155E" />
+                </button>
+            </div>
+            <div 
+                className="collapse-div-body"
+                style={styles.collapseStyle}
+            >   <div className="collapse-div-body-header">
+                    <Typography variant="h6">Additional Member Information</Typography>
+                </div>
+                 <Typography variant="body2">{`Age: ${member.age}`}</Typography>
+                 <Typography variant="body2">{`Gender: ${member.gender}`}</Typography>
+                 <Typography variant="body2">{`Birthdate: ${member.bdate}`}</Typography>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/components/user/household.js
+++ src/components/user/household.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
-import './user.css';
+import './household.css'
 import Navbar from '../navbar/emptyNavbar';
 import Sidebar from '../sidebar/userSidebar';
 import PhoneNav from '../navbar/phoneNavbar';
 import FloatingActions from '../navbar/floatingActions';
 import RequestCertificateModal from '../modal/requestCertificate'
 import { Typography } from '@material-ui/core';
+import {IoMdAdd} from 'react-icons/io'
+import CollapseDiv from '../collapse/collapseDiv';
 
 
-
-
-export default function Household(props) {
+export default function Household() {
     const [certificateModal,setCertificateModal] = useState(false);
     const screenWidth = window.innerWidth;
 
-
+    const houseHold = [
+      {id: 1,role:'Father', gender:'Male', name:"Justin Doe", age:42, bdate: "September 21, 1984"},
+      {id: 2,role:'Mother', gender:'Female', name:"Jane Doe", age:39, bdate :"September 21, 1981"},
+      {id: 3,role:'Child', gender:'Male', name:"John Doe", age:23, bdate: "September 21, 1997"},
+      {id: 4,role:'Child', gender:'Female', name:"Judy Doe", age:21, bdate: "September 21, 1999"},
+      {id: 5,role:'Child', gender:'Male', name:"Jonas Doe", age:19, bdate: "September 21, 2001"},
+      {id: 6,role:'Child', gender:'Male', name:"Jonas Doe", age:19, bdate: "September 21, 2001"},
+      {id: 7,role:'Child', gender:'Male', name:"Jonas Doe", age:19, bdate: "September 21, 2001"},
+    ]
     return (
         <>
          <Navbar />
@@ -38,9 +46,35 @@ export default function Household(props) {
                 {/* ========================content goes here======================= */}
                     <div className="household-wrapper">
                         <div className="household-id">
-                        <Typography variant="h6">My Household Id: HSE-5687846868-12</Typography>
+                          <Typography variant="h6">Household Id: HSE-5687846868-12</Typography>
                         </div>
-                        <Typography>Set up your Household</Typography>
+                        <div className="household-wrapper-content">
+                          <div className="household-header-content">
+                            <Typography
+                              variant="body1"
+                              // color="textSecondary"
+                            >Details of your Household Members</Typography>
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                            >(Detalye sa imong membro sa panimalay)</Typography>
+                          </div>
+                          <div className="household-body-content">
+                              <form>
+                                <div className="household-body-content-heading">
+                                  <button className="btn-add-member">
+                                    <span><IoMdAdd size={24} color={"#9F0710"} /></span>
+                                    Add a Member
+                                  </button>
+                                </div>
+                                {
+                                  houseHold.map((member,index)=>{
+                                    return <CollapseDiv member={member} key={index} />
+                                  })
+                                }
+                              </form>
+                          </div>
+                        </div>
                     </div>
                  {/* =======================end of content here===================== */}
               </div>
