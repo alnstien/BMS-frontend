0d3acbe5f9e8d24a3f795a2fd3e327208ffaeeaf
alnstien <alnstien@yahoo.com>
2021-05-24 18:55:30 +0800

    done with requests

--- src/components/card/requirementsCard.js
+++ src/components/card/requirementsCard.js
@@ -1,11 +1,14 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
 
-export default function RequirementCard({requirement}) {
+export default function RequirementCard({requirements}) {
     return (
         <div className="req-card">
             <Typography>
-                {requirement}
+                {requirements.required}
+               <div>
+                    <input type="file" name={requirements.fileId} id={requirements.fileId} required />
+               </div>
             </Typography>
         </div>
     )
--- src/components/user/certificate.js
+++ src/components/user/certificate.js
@@ -3,7 +3,7 @@ import Navbar from '../navbar/emptyNavbar';
 import Sidebar from '../sidebar/userSidebar';
 import PhoneNav from '../navbar/phoneNavbar';
 import './certificate.css';
-import { FormGroup, Typography,MenuItem,FormControl,InputLabel,Select,TextField } from '@material-ui/core';
+import { FormGroup, Typography,MenuItem,FormControl,InputLabel,Select,TextField,Button } from '@material-ui/core';
 import RequirementCard from '../card/requirementsCard';
 
 
@@ -14,34 +14,69 @@ export default function Certificate(props) {
   const [approval,setApproval] = useState(false)
   const [acceptance,setAcceptance] = useState(false)
   const [land,setLand] = useState(false)
-
+  const [requirements,setRequirements] =useState([])
 
   const handleSelect=(e)=>{
     if(e.target.value==="others"){
       setOthers(true)
       setApproval(false);
       setAcceptance(false)
-      setLand(false)
+      setLand(false);
+      setRequirements([]);
     }else if(e.target.value==="certificate_of_cutting_trees"){
+      setRequirements([
+        {required:'Purok Clearance',fileId:'purok_clearance'},
+        {required:'OR Receipt',fileId:'or_receipt'}
+      ])
       setApproval(true);
       setOthers(false)
       setLand(false)
       setAcceptance(false)
+    }else if(e.target.value==="barangay_certification_of_residency" ){
+      setRequirements([{required:'OR Receipt',fileId:'or_receipt'}])
+      setApproval(false);
+      setOthers(false)
+      setLand(false)
+      setAcceptance(false)
+    }else if(e.target.value==="certification_of_indegency" ){
+      setRequirements([{required:'OR Receipt',fileId:'or_receipt'}])
+      setApproval(false);
+      setOthers(false)
+      setLand(false)
+      setAcceptance(false)
+    }else if(e.target.value==="barangay_clearance"){
+      setRequirements([
+      {required:'Purok Clearance',fileId:'purok_clearance'},
+      {required:'OR Receipt',fileId:'or_receipt'}
+    ])
+      setApproval(false);
+      setOthers(false)
+      setLand(false)
+      setAcceptance(false)
+    }else if(e.target.value==="certification_of_good_moral"){
+      setRequirements([
+        {required:'Purok Clearance',fileId:'purok_clearance'},
+        {required:'OR Receipt',fileId:'or_receipt'}
+      ])
+      setApproval(false);
+      setOthers(false)
+      setLand(false)
+      setAcceptance(false)
     }else if(e.target.value==="letter_of_acceptance"){
+      setRequirements([
+        {required:'Latest RT PCR or Antigen Test Negative Result',fileId:'purok_clearance'},
+        {required:'OR Receipt',fileId:'or_receipt'}
+      ])
       setAcceptance(true)
       setApproval(false);
       setOthers(false)
       setLand(false)
-    }else if(e.target.value==="barangay_certification_of_land_ownership"){
-      setAcceptance(false)
-      setLand(true)
-      setApproval(false);
-      setOthers(false)
     }else{
       setOthers(false)
       setApproval(false);
       setAcceptance(false)
       setLand(false)
+      setRequirements([]);
     }
 }
     return (
@@ -64,8 +99,8 @@ export default function Certificate(props) {
               <div className="user-root">
                 {/* ========================content goes here======================= */}
                 <div className="file-req-main-wrapper">
-                    <Typography variant="h5">Request a Certificate</Typography>
-                    <form>
+                    <Typography variant="h4">Request a Certificate</Typography>
+                    <form >
                     <FormGroup>
                         <FormControl style={{marginTop:20}}>
                           <InputLabel 
@@ -76,6 +111,7 @@ export default function Certificate(props) {
                             labelId="demo-simple-select-helper-label"
                             id="demo-simple-select-helper"
                             color="secondary"
+                            required
                             onChange={e=>handleSelect(e)}
                           >
                             <MenuItem value="">
@@ -143,13 +179,32 @@ export default function Certificate(props) {
                           <TextField
                             color="secondary"
                             label="Lokasyon kung asa ka gikan"
+                            required
                           />
                         </FormControl>
                         </>
                         : null
                       }
-                          <RequirementCard requirement={"Purok Clearance"} />
-                          <RequirementCard requirement={"Valid OR"} />
+                        {
+                          requirements.length <=0 ? 
+                          null
+                          :
+                          <FormControl style={{marginTop:10}}>
+                            <em><Typography color="secondary" variant="h5">Requirement/s</Typography></em>
+                          </FormControl>
+                        }
+                          {
+                            requirements.map((req,index)=>{
+                              return <RequirementCard requirements={req} key={index} />
+                            })
+                          }
+                          <FormControl style={{marginTop:10}}>
+                            <div className="req-submit-footer">
+                              <Button variant="contained" color="secondary" type="submit">
+                                Send Request
+                              </Button>
+                            </div>
+                          </FormControl>
                       </FormGroup>
                     </form>
                 </div>
