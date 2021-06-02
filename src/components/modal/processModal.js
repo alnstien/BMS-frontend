d5a8754d415affdf6308943697768f7863dabce7
alnstien <alnstien@yahoo.com>
2021-06-02 10:18:25 +0800

    almost

--- src/components/modal/processModal.js
+++ src/components/modal/processModal.js
@@ -6,6 +6,7 @@ import {flexContent,flexoverlay,fulloverlay,content,width} from '../../config/se
 import {Typography} from '@material-ui/core'
 import ProImg from '../../assets/avatar.jpg'
 import ProQR from '../../assets/qr.png'
+import ORImage from '../../assets/or.png'
 
 
 export default function CreatePostModal({show,setShow}) {
@@ -25,10 +26,12 @@ export default function CreatePostModal({show,setShow}) {
         }}
      >
         <div className="flex-modal">
-            <button
-                 className="modal-close"
-                onClick={()=>setShow(!show)}
-            >&times;</button>
+            <div className="flex-modal-header-sticky">
+                <button
+                    className="modal-close"
+                    onClick={()=>setShow(!show)}
+                >&times;</button>
+            </div>
             <div className="flex-modal-content">
                 <div className="modal-process-header">
                     <div className="process-user-info">
@@ -37,12 +40,11 @@ export default function CreatePostModal({show,setShow}) {
                             <div className="process-user-classification">
                                 <p>Resident</p>
                             </div>
-                            <Typography variant="h5">CERTIFICATION OF INDIGENCY</Typography>
+                            <Typography variant="h5">BARANGAY CERTIFICATE</Typography>
                             <Typography color="textSecondary"><b>Date: </b>September 12, 2022</Typography>
                             <Typography><b>Name: </b>Jane Doe</Typography>
                             <Typography><b>Purok: </b>Purok 1</Typography>
-                            <Typography><b>Household ID: </b>HSE-5687846868-12</Typography>
-                            <Typography><b>Name: </b>Jane Doe</Typography>
+                            <Typography><b>Household Number: </b>HSE-5687846868-12</Typography>
                         </div>
                     </div>
                     <div className="process-user-qr">
@@ -56,8 +58,18 @@ export default function CreatePostModal({show,setShow}) {
                        </form>
                     </div>
                 </div>
-                <div className="flex-modal-body">
-                    
+                <div className="modal-process-header">
+                    <div className="inline-column mt-10">
+                        <Typography variant="h5" color="secondary">Requirement/s Verification</Typography>
+                        <Typography>2 Files Attached</Typography>
+                        <a href={ORImage} target="_blank">Photo of Issued OR</a>
+                        <a href={ProImg} target="_blank">Photo of Purok Clearance</a>
+                    </div>
+                </div>
+                <div className="modal-process-footer mt-10">
+                    <div className="inline-column mt-10">
+                        <Typography variant="h5">Upload your Files</Typography>
+                    </div>
                 </div>
             </div>
         </div>
